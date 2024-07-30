28da065ce55bb31b7c2a29478247a7cde44eab80
GerenP <147972228+GerenP@users.noreply.github.com>
2024-07-30 11:33:51 +0700

    initialize

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
